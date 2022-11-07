ada67c95cd14ba1d4d446947a97a82f5e20fb478
Jose <ajose9975@gmail.com>
2022-11-07 13:57:04 -0300

    Add project files.

---
+++ Cap9/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap9.Entities.Enums {
+    internal enum OrderStatus : int {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,  
+        Delivered = 3
+    }
+}
